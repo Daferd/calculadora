7a681580e00e47616052bad95ebbbfdf6487514a
AlexisBerrio <bealexis@hotmail.com>
2020-09-30 12:41:41 -0500

    App icon added

--- app/src/main/java/com/daferarevalo/calculadora/MainActivity.kt
+++ app/src/main/java/com/daferarevalo/calculadora/MainActivity.kt
@@ -1,22 +1,37 @@
+/*
+Calculadora App
+Autores:
+           Dario Fernando Arévalo
+           Alexis Berrio Arenas
+ */
 package com.daferarevalo.calculadora
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val SUMA = 1
+        const val RESTA = 2
+        const val MULTIPLICACION = 3
+        const val DIVISION = 4
+        const val NO_OPERACION = 0
+        private const val EMPTY = ""
+    }
+
     private var num1: Double = 0.0
     private var num2: Double = 0.0
     private var operacion: Int = 0
-    private var band: Int = 0
     private var result: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Botones para números
         unoButton.setOnClickListener { numeroPresionado("1") }
         dosButton.setOnClickListener { numeroPresionado("2") }
         tresButton.setOnClickListener { numeroPresionado("3") }
@@ -27,16 +42,19 @@ class MainActivity : AppCompatActivity() {
         ochoButton.setOnClickListener { numeroPresionado("8") }
         nueveButton.setOnClickListener { numeroPresionado("9") }
         ceroButton.setOnClickListener { numeroPresionado("0") }
+
+        // Botones para operaciones
         puntoButton.setOnClickListener { numeroPresionado(".") }
         masButton.setOnClickListener { operacionPresionada(SUMA) }
         menosButton.setOnClickListener { operacionPresionada(RESTA) }
         dividirButton.setOnClickListener { operacionPresionada(DIVISION) }
         multiplicarButton.setOnClickListener { operacionPresionada(MULTIPLICACION) }
 
+        // Botón para borrar
         borrarButton.setOnClickListener {
             num1 = 0.0
             num2 = 0.0
-            resultadoTextView.text = ""
+            resultadoTextView.text = EMPTY
             operacion = NO_OPERACION
         }
 
@@ -71,19 +89,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
     private fun operacionPresionada(operacion: Int){
-        this.operacion=operacion
-        resultadoTextView.text = ""
+        this.operacion = operacion
+        resultadoTextView.text = EMPTY
     }
 
-    companion object {
-        const val SUMA = 1
-        const val RESTA = 2
-        const val MULTIPLICACION = 3
-        const val DIVISION = 4
-        const val NO_OPERACION = 0
-    }
 }
 
 
